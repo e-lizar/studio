7aaca35b1028b963e0d24df5d8cbedce2a54990c
Elizabeth Kamau <lizkamau55@gmail.com>
2025-11-28 06:15:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1239218959-2dee6",
+  "appId": "1:914548259516:web:ba33dd087be5f577846eb9",
+  "apiKey": "AIzaSyBAsp0SEtKUho4sWEPhP3k_7gRcO-vAuHc",
+  "authDomain": "studio-1239218959-2dee6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "914548259516"
+};
